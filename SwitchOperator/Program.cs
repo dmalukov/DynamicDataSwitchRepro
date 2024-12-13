fd618da31b0c5ebae9dd62271dcdd42f97e4deed
dm <dm@example.com>
2024-12-13 16:22:38 +0200

    init

---
+++ SwitchOperator/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DynamicData;
+using DynamicData.Cache.Internal;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+Console.WriteLine("Hello, World!");
+
+
+
+public static class DynamicDataExt
+{
+    public static IObservable<IChangeSet<TObject, TKey>> SwitchChangeSet<TObject, TKey>(
+        this IObservable<IObservable<IChangeSet<TObject, TKey>>> sources
+    )
+        where TObject : notnull
+        where TKey : notnull
+    {
+        return Observable.Create<IChangeSet<TObject, TKey>>(
+            observer =>
+            {
+                object locker = new();
+
+                LockFreeObservableCache<TObject, TKey> destination = new();
+
+                IObservable<IChangeSet<TObject, TKey>> populator = Observable.Switch(
+                        sources.Do(
+                            _ =>
+                            {
+                                lock (locker)
+                                {
+                                    destination.Clear();
+                                }
+                            })
+                    )
+                    .Synchronize(locker)
+                    .Do(changes => destination.Edit(updater => updater.Clone(changes)))
+                    .Where(_ => false);
+
+                return new CompositeDisposable(
+                    destination,
+                    populator.Merge(destination.Connect()).SubscribeSafe(observer)
+                );
+            });
+    }
+}
---
+++ TestSwitchOperator/SwitchTest.cs
@@ -0,0 +1,64 @@
+﻿using DynamicData;
+using System.Reactive.Linq;
+
+namespace TestSwitchOperator
+{
+    public class SwitchTest
+    {
+
+        [Fact]
+        public void PropagatesErrors()
+        {
+            bool exceptionCaught = false;
+
+            List<List<int>> listOfLists = [[0, 1, 2]];
+
+            using IDisposable subscription = listOfLists.ToObservable()
+                .ToObservableChangeSet(int (value) => value)
+                .Select(set =>
+                {
+                    throw new Exception();              // Something bad happened here
+                    return Observable.Return(set);
+                })
+                .Do(_ => { }, e => { })                 // here the error handler is called
+                .Switch()                               // here the exception is swallowed
+                .Catch((Exception e) =>                 // this code will never be executed
+                {
+                    exceptionCaught = true;
+                    return Observable.Empty<IChangeSet<int, int>>();
+                })
+                .Subscribe();
+
+            Assert.True(exceptionCaught);
+        }
+
+
+
+        [Fact]
+        public void FixedPropagatesErrors()
+        {
+            bool exceptionCaught = false;
+
+            List<List<int>> listOfLists = [[0, 1, 2]];
+
+            using IDisposable subscription = listOfLists.ToObservable()
+                .ToObservableChangeSet(int (value) => value)
+                .Select(set =>
+                {
+                    throw new Exception();
+                    return Observable.Return(set);
+                })
+                .Do(_ => { }, e => { })
+                .SwitchChangeSet()
+                .Catch((Exception e) =>
+                {
+                    exceptionCaught = true;
+                    return Observable.Empty<IChangeSet<int, int>>();
+                })
+                .Subscribe();
+
+            Assert.True(exceptionCaught);
+        }
+
+    }
+}
